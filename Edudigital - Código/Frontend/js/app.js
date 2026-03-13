68371cff376a45633f21bb89622de7b490fbe16e
l-bonfim <l_bonfim@hotmail.com>
2026-03-13 20:37:37 -0300

    implementado login de usuarios, agora as funcoes responsaveis pelo registro e login possuem script proprio fora de app.js

--- Edudigital - Código/Frontend/js/app.js
+++ Edudigital - Código/Frontend/js/app.js
@@ -112,24 +112,3 @@ function responderPergunta(pergunta) {
     }
 
 }
-
-document.getElementById("reg-form").addEventListener("submit", async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData);
-    const response = await fetch("http://127.0.0.1:5000/usuarios", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    })
-    const fetchedData = await response.json()
-    
-    if(fetchedData.erro !== undefined) {
-        const errorMessage = document.getElementById("mensagem")
-        errorMessage.innerHTML = fetchedData.erro
-    }
-    
-})
